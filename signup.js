3a8e6817fc3476d9fe34c537a1dc1f9d6ae111c7
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2023-01-30 13:51:06 -0500

    Script compares passwords before submit, styles

--- signup.js
+++ signup.js
@@ -3,6 +3,9 @@
 const passwordInput = document.getElementById('password');
 const passwordBox = document.getElementById('password-box');
 const passwordRequirements = `<ul id="requirements"><li>5-16 characters</li><li>1 number</li><li>1 uppercase character</li><li>1 lowercase character</li></ul>`;
+const confirmPasswordInput = document.getElementById('password-confirm');
+const confirmPasswordBox = document.getElementById('confirm-password-box');
+const passwordDoesNotMatch = `<ul id="mismatch"><li>Password does not match.</li></ul>`
 const formFields = document.getElementById('input-box');
 
 formFields.addEventListener('focusout', (e) => {
@@ -10,19 +13,41 @@ formFields.addEventListener('focusout', (e) => {
     console.log(field);
     console.log(field.checkValidity());
     let requirementsList = document.getElementById('requirements');
+    let passwordMismatchTip = document.getElementById('mismatch');
+
     if (!field.checkValidity()) {
         field.classList.add('required');
         if (field.id === 'password') {
             if (!passwordBox.contains(requirementsList)) {
                 passwordBox.insertAdjacentHTML('beforeend', passwordRequirements);
-            } else {
-                return;
             }
         }
     } else {
         field.classList.remove('required');
         if (field.id === 'password') {
             passwordBox.removeChild(requirementsList);
+        }
+    }
+
+    if (field.id === 'password-confirm') {
+        if (!(field.value === passwordInput.value)) {
+            field.classList.add('required');
+            if (!(confirmPasswordBox.contains(passwordMismatchTip)))
+            confirmPasswordBox.insertAdjacentHTML('beforeend', passwordDoesNotMatch);
+        } else {
+            confirmPasswordBox.removeChild(passwordMismatchTip);
+            field.classList.remove('required');
+        }
+    }
+
+    if (field.id === 'password') {
+        if (!(field.value === confirmPasswordInput.value)) {
+            confirmPasswordInput.classList.add('required');
+            if (!(confirmPasswordBox.contains(passwordMismatchTip)))
+            confirmPasswordBox.insertAdjacentHTML('beforeend', passwordDoesNotMatch);
+        } else {
+            confirmPasswordBox.removeChild(passwordMismatchTip);
+            confirmPasswordInput.classList.remove('required');
         }
     }
     return;
