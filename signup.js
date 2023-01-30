c4593d8dda4185947fbde78d2b929d993458b97f
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2023-01-30 10:59:41 -0500

    Password field removes requirements when valid
    Two explicit functions to append and remove the password requirements
    list were removed for the more elegant solution of capturing all
    focusout events on the input-box div and executing the appropriate
    logic.

--- signup.js
+++ signup.js
@@ -2,21 +2,25 @@
 
 const passwordInput = document.getElementById('password');
 const passwordBox = document.getElementById('password-box');
-const passwordRequirements = `<ul id="requirements"><li>5-16 characters</li><li>1 number</li><li>1 uppercase character</li><li>1 lowercase character</li></ul>`
+const passwordRequirements = `<ul id="requirements"><li>5-16 characters</li><li>1 number</li><li>1 uppercase character</li><li>1 lowercase character</li></ul>`;
+const formFields = document.getElementById('input-box');
 
-passwordInput.addEventListener('invalid', () => {
-    let requirementsList = document.getElementById('requirements');
-    if (!passwordBox.contains(requirementsList)) {
-        passwordBox.insertAdjacentHTML('beforeend', passwordRequirements);
+formFields.addEventListener('focusout', (e) => {
+    let field = e.target;
+    console.log(field);
+    console.log(field.checkValidity());
+    if (!field.checkValidity()) {
+        field.classList.add('required');
+    } else {
+        field.classList.remove('required');
+    }
+    if (field.id === 'password') {
+        let requirementsList = document.getElementById('requirements');
+        if (!passwordBox.contains(requirementsList)) {
+            passwordBox.insertAdjacentHTML('beforeend', passwordRequirements);
+        } else {
+            passwordBox.removeChild(requirementsList);
+        }
     }
     return;
 })
-
-passwordInput.addEventListener('valid', () => {
-    let requirementsList = document.getElementById('requirements');
-    if (passwordBox.contains(requirementsList)) {
-        passwordBox.removeChild(passwordRequirements);
-    }
-    return;
-})
-
